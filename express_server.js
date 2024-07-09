7290613b75fc3c902704c68da384787be9625ba8
K-Fort <fortink01@gmail.com>
2024-07-09 10:23:39 -0400

    added post request for url, generating random string of 6 characters for tinyURL

--- express_server.js
+++ express_server.js
@@ -2,14 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-
 app.set("view engine", "ejs");
-
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+// Function to generate a random string of 6 alphanumeric characters
+const generateRandomString = () =>
+  Array.from({ length: 6 }, () =>
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      .charAt(Math.floor(Math.random() * 62))
+  ).join('');
+
+// A good rule of thumb to follow is that routes should be ordered from most specific to least specific.
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -37,6 +46,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
@@ -44,8 +57,17 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+``
+
 
 
